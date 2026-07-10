57be5cc5915d39d235fa91b97d70197409cc7f96
Minh Doan <minhdpc@gmail.com>
2026-07-10 16:28:51 +0700

    Refactor to return SRC and APP path

--- src/app/constants/defaults.ts
+++ src/app/constants/defaults.ts
@@ -1,7 +1,12 @@
 import { getRootPath } from "./concern";
+import path from 'path';
 
 const ROOT_DIR = getRootPath(__dirname, '.app_root');
+const SRC_DIR = path.resolve(ROOT_DIR, 'src');
+const APP_DIR = path.resolve(SRC_DIR, 'app');
 
 export const Defaults = {
-  ROOT_DIR
+  ROOT_DIR,
+  SRC_DIR,
+  APP_DIR
 } as const
--- src/server.ts
+++ src/server.ts
@@ -2,5 +2,5 @@ import { appConstant } from "src/app";
 
 console.log("Template for NodeJS project with TypeScript and ExpressJS");
 
-const ROOT_DIR = appConstant.Defaults.ROOT_DIR
-console.log(`Root directory: ${ROOT_DIR}`);
+const defaultConst = appConstant.Defaults;
+console.log(defaultConst);
