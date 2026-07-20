5d84db593289ef8d7076a6cdf0972993aa8a21b5
Minh Doan <minhdpc@gmail.com>
2026-07-20 23:40:08 +0700

    refactor: remove bootstrap file and its dependencies

--- src/bootstrap.ts
+++
@@ -1,6 +0,0 @@
-require('src/app/config').default.__configure('./config');
-require("src/constants");
-require('src/utils');
-
-// pre-load constants / config / utils namespaces
-require('src/app');
